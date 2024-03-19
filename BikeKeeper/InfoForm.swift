d72804a737d4f7bfffca3832cfe59b7f5df0167f
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2024-03-18 21:59:53 -0700

    Update InfoForm.swift

--- BikeKeeper/InfoForm.swift
+++ BikeKeeper/InfoForm.swift
@@ -18,47 +18,54 @@ struct InfoForm: View {
         }
     
     var body: some View {
-        NavigationView{
         GeometryReader{ geometry in
             ZStack{
                 Color(UIColor.systemGroupedBackground)
-            VStack{
+                VStack{
                     VStack{
+                        Text("Information")
+                            .position(x:0.5*geometry.size.width, y:0.2*geometry.size.height)
+                            .font(.title)
+                            .bold()
+                        
                         Form{
                             Section{
                                 TextField("First Name", text: $firstName)
                                 TextField("Last Name", text: $lastName)
                             }
-                        }.position(x: 0.5*geometry.size.width, y: geometry.size.height*0.3)
+                        }.position(x: 0.5*geometry.size.width, y: geometry.size.height*0.1)
                             .scrollContentBackground(.hidden)
                             .background(
                                 Color(UIColor.systemGroupedBackground))
+                            .frame(height: geometry.size.height*0.19)
                         
                         Button(action: {}, label: {
                             NavigationLink(destination: IntroPage(), label:{
-                                    Text("Continue")})
+                                Text("Continue").foregroundColor(.white)})
                         })
                         .buttonStyle(.borderedProminent)
                         .controlSize(.large)
+                        
                     }.background(Color(UIColor.systemGroupedBackground))
+                    
+                        .position(x: geometry.size.width/2, y: geometry.size.height * 0.15)
+                    Image("FormImg")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width, height: 0.6*geometry.size.height, alignment: .centerFirstTextBaseline)
+                        .position(x:geometry.size.width*0.5, y: geometry.size.height*0.3)
+                        .ignoresSafeArea(.keyboard)
                 }
-                .position(x: geometry.size.width/2, y: geometry.size.height * 0.15)
-                Image("FormImg")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: geometry.size.width, height: 0.6*geometry.size.height, alignment: .centerFirstTextBaseline)
-                    .position(x:geometry.size.width*0.5, y: geometry.size.height*0.3)
-                    .ignoresSafeArea(.keyboard)
-            }.navigationTitle("Information")
-            }
+            }}
+            
             .onAppear {
                 UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation") // Forcing the rotation to portrait
                 AppDelegate.orientationLock = .portrait // And making sure it stays that way
             }.onDisappear {
                 AppDelegate.orientationLock = .all // Unlocking the rotation when leaving the view
             }
-            }
             .ignoresSafeArea()
+            
     }
     
     
